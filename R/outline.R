8d97b942118587a11a61bda953193566d60eac33
John Benninghoff <jbenninghoff@mac.com>
2022-05-08 18:54:28 -0500

    Update render_timevis() docs

--- R/outline.R
+++ R/outline.R
@@ -3,13 +3,13 @@
 #' Create a timeline visualization using [timevis::timevis()] and save using
 #'   [htmlwidgets::saveWidget()].
 #'
-#' `render_timevis()` saves the widget with a fixed width (1400, the width of my laptop screen) and
-#'   returns an `htmlwidgets` object without a specified width.
+#' `render_timevis()` saves the widget with a fixed width (1400, the approximate width of my laptop
+#'   screen) and returns an `htmlwidgets` object without a specified width.
 #'
 #' @inheritParams timevis::timevis
 #' @inheritParams htmlwidgets::saveWidget
 #'
-#' @return A timeline visualization `htmlwidgets` object
+#' @return A timeline visualization `htmlwidgets` object.
 #' @export
 render_timevis <- function(data, groups, file, showZoom = FALSE) { # nolint: object_name_linter
   timevis::timevis(data, groups = groups, showZoom = showZoom, width = 1400) |>
